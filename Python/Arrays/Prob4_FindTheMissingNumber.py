af36a1d7473e9720093615e5d90e61004b4a27ed
yellala vishnu vardhan reddy <y.vishnu55@gmail.com>
2023-10-15 22:49:39 +0530

    Added python code for finding missing number

--- Python/Arrays/Prob4_FindTheMissingNumber.py
+++ Python/Arrays/Prob4_FindTheMissingNumber.py
@@ -5,3 +5,18 @@ Given an array arr[] of size N-1 with integers in the range of [1, N],
 the task is to find the missing number from the first N integers.
 '''
 
+# 1. Finding array sum.
+# Time complexity -> O(n), Space Complexity -> O(1)
+
+
+def findTheMissingNumberUsingSum(arr):
+    l = len(arr)
+    sum = 0
+    for i in range(l):
+        sum += arr[i]
+    return (l + 2) * (l + 1) // 2 - sum
+
+if __name__ == "__main__":
+    arr = [1, 2, 4, 6, 3, 7, 8]
+    # Finding array sum
+    print("Missing number is:", findTheMissingNumberUsingSum(arr))
